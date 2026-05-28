c12f1dd4cd2e3aea67035ca3e40bd134c512d9e5
Vrmahesh2035 <vrmahesh2035@gmail.com>
2026-05-28 08:51:25 +0530

    Update MySQL connection settings in config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     # ── MySQL connection ──────────────────────────────────────────
     MYSQL_HOST     = os.getenv("MYSQL_HOST",     "localhost")
-    MYSQL_PORT     = int(os.getenv("MYSQL_PORT", "3306"))
+    MYSQL_PORT     = int(os.getenv("MYSQL_PORT", "port"))
     MYSQL_USER     = os.getenv("MYSQL_USER",     "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "Vrmahesh@19")          # change to your password
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")          # change to your password
     MYSQL_DB       = os.getenv("MYSQL_DB",       "university_club_db")
 
     SECRET_KEY     = os.getenv("SECRET_KEY",     "ucms-secret-key-2025")
